b597a941103cef27cb3f0ad7fafca4ec6a519744
Mauro Busso <mauro.busso12@gmail.com>
2022-10-19 15:48:59 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
      const people = document.querySelector('input').value //birthday name
      try{
-         const response = await fetch(`https://birthday-api.onrender.com/api/${people}`) //birthday
+         const response = await fetch(`https://birthday-api.onrender.com/${people}`) //birthday
          const data = await response.json()
 
          console.log(data)
