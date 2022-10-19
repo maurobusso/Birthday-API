adbffd21cda45b39ef98dfa7cf5e6bd44fa4766f
Mauro Busso <mauro.busso12@gmail.com>
2022-10-19 16:24:02 +0100

    changsmall changeses

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
-     const people = document.querySelector('input').value //birthday name
+     const people = document.querySelector('input').value 
      try{
-         const response = await fetch(`https://birthday-api.onrender.com/api/${people}`) //birthday
+         const response = await fetch(`https://birthday-api.onrender.com/api/${people}`) 
          const data = await response.json()
 
          console.log(data)
-         document.querySelector('h2').innerText = data //birthday name
+         document.querySelector('h2').innerText = data 
      }catch(error){
          console.log(error)
      }
