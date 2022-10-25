80f2e64599d403ad7496001868971a59287ee85a
Mauro Busso <mauro.busso12@gmail.com>
2022-10-25 21:47:18 +0100

    changes to client side js

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
      const friendName = document.querySelector('input').value 
      try{
-         const response = await fetch(`https://birthday-api.onrender.com/${friendName}`) 
-         const data = await response.json()
+         const apiData = await fetch(`https://birthday-api.onrender.com/${friendName}`) 
+         console.log(apiData)
+         const data = await response.json(apiData)
 
          console.log(data)
-         document.querySelector('h2').innerText = data.birthday
+         document.querySelector('h2').innerText = data.birthName
      }catch(error){
          console.log(error)
      }
