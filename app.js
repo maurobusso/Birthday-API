4a7375c82cff2bb1875da465cfb2ee3b5b18fd0d
Mauro Busso <mauro.busso12gmail.com>
2023-02-22 15:35:42 +0000

    changes

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const button = document.querySelector('button')  
-const container = document.getElementById('container')
-const friend = document.getElementById('myInput')
+// const button = document.querySelector('button')  
+// const container = document.getElementById('container')
+// const friend = document.getElementById('myInput')
 
-button.addEventListener('click', apiRequest)
+// button.addEventListener('click', apiRequest)
 
-async function apiRequest(){
-    console.log('works')
-    const friendName = friend.value 
-    try{
-        const response = await fetch(`https://birthday-api.onrender.com/api/${friendName}`) 
-        const data = await response.json()
+// async function apiRequest(){
+//     console.log('works')
+//     const friendName = friend.value 
+//     try{
+//         const response = await fetch(`https://birthday-api.onrender.com/api/${friendName}`) 
+//         const data = await response.json()
 
-        console.log(data)
-        document.querySelector('h2').innerText = data.birthday
-    }catch(error){
-        console.log(error)
-    }
-    console.log(friendName)
-}
+//         console.log(data)
+//         document.querySelector('h2').innerText = data.birthday
+//     }catch(error){
+//         console.log(error)
+//     }
+//     console.log(friendName)
+// }
--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-// const button = document.querySelector('button')  
-// const container = document.getElementById('container')
-// const friend = document.getElementById('myInput')
+const button = document.querySelector('button')  
+const container = document.getElementById('container')
+const friend = document.getElementById('myInput')
 
-// button.addEventListener('click', apiRequest)
+button.addEventListener('click', apiRequest)
 
-// async function apiRequest(){
-//     const friendName = friend.value 
-//     try{
-//         const response = await fetch(`https://birthday-api.onrender.com/api/${friendName}`) 
-//         const data = await response.json()
+async function apiRequest(){
+    const friendName = friend.value 
+    try{
+        const response = await fetch(`https://birthday-api.onrender.com/api/${friendName}`) 
+        const data = await response.json()
         
-//         console.log(data)
-//         document.querySelector('h2').innerText = data.birthday
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthday
+    }catch(error){
+        console.log(error)
+    }
+}
