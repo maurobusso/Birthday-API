118da94ca9f6a8b5908f7c6bfa6f92e8c684206f
Mauro Busso <mauro.busso12gmail.com>
2023-02-22 14:44:07 +0000

    changes

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('button')  
+const container = document.getElementById('container')
+const friend = document.getElementById('myInput')
+
+button.addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const friendName = friend.value 
+    try{
+        const response = await fetch(`https://birthday-api.onrender.com/api/${friendName}`) 
+        const data = await response.json()
+        
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthday
+    }catch(error){
+        console.log(error)
+    }
+}
