9447c4cdd67ede64219c6b43822dd40229e2ff19
Mauro Busso <mauro.busso12gmail.com>
2023-03-08 10:59:03 +0000

    url changes

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ async function apiRequest(){
     console.log('click')
     const friendName = friend.value 
     try{
-        const response = await fetch(`https://birthday-api.onrender.com/api/${friendName}`) 
+        const response = await fetch(`https://funny-muffin-ce082f.netlify.app/${friendName}`) 
         const data = await response.json()
         
         container.innerText = data.birthday
@@ -16,3 +16,6 @@ async function apiRequest(){
         console.log(error)
     }
 }
+
+`https://funny-muffin-ce082f.netlify.app/${friendName}`
+//`https://birthday-api.onrender.com/api/${friendName}`
